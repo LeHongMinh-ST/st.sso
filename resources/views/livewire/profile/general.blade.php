c79bdcc427879098fdc0d436860c5f82f8cd26ef
minhlh <minhlh@haposoft.com>
2025-03-13 17:18:35 +0700

    refactor: auth

--- resources/views/livewire/profile/general.blade.php
+++ resources/views/livewire/profile/general.blade.php
@@ -8,96 +8,76 @@
             <div class="row">
                 <div class="col-lg-6">
                     <div class="mb-3">
-                        <label class="form-label">Username</label>
-                        <input type="text" value="Victoria" class="form-control">
+                        <label class="form-label">Tên đăng nhập</label>
+                        <input type="text" wire:model="user_name" placeholder="Nhập tên đăng nhập" class="form-control">
+                        @error('user_name')
+                        <label id="error-user_name"
+                               class="validation-error-label text-danger"
+                               for="user_name">{{ $message }}</label>
+                        @enderror
                     </div>
                 </div>
-                <div class="col-lg-6">
-                    <div class="mb-3">
-                        <label class="form-label">Full name</label>
-                        <input type="text" value="Smith" class="form-control">
-                    </div>
-                </div>
-            </div>
-
-            <div class="row">
-                <div class="col-lg-6">
-                    <div class="mb-3">
-                        <label class="form-label">Address line 1</label>
-                        <input type="text" value="Ring street 12" class="form-control">
-                    </div>
-                </div>
-                <div class="col-lg-6">
-                    <div class="mb-3">
-                        <label class="form-label">Address line 2</label>
-                        <input type="text" value="building D, flat #67" class="form-control">
-                    </div>
-                </div>
-            </div>
-
-            <div class="row">
-                <div class="col-lg-4">
-                    <div class="mb-3">
-                        <label class="form-label">City</label>
-                        <input type="text" value="Munich" class="form-control">
-                    </div>
-                </div>
-                <div class="col-lg-4">
-                    <div class="mb-3">
-                        <label class="form-label">State/Province</label>
-                        <input type="text" value="Bayern" class="form-control">
-                    </div>
-                </div>
-                <div class="col-lg-4">
-                    <div class="mb-3">
-                        <label class="form-label">ZIP code</label>
-                        <input type="text" value="1031" class="form-control">
-                    </div>
-                </div>
-            </div>
-
-            <div class="row">
                 <div class="col-lg-6">
                     <div class="mb-3">
                         <label class="form-label">Email</label>
-                        <input type="text" readonly="readonly" value="victoria@smith.com" class="form-control">
-                    </div>
-                </div>
-                <div class="col-lg-6">
-                    <div class="mb-3">
-                        <label class="form-label">Your country</label>
-                        <select class="form-select">
-                            <option value="germany" selected="">Germany</option>
-                            <option value="france">France</option>
-                            <option value="spain">Spain</option>
-                            <option value="netherlands">Netherlands</option>
-                            <option value="other">...</option>
-                            <option value="uk">United Kingdom</option>
-                        </select>
-                    </div>
+                        <input type="text" wire:model="email" placeholder="Nhập email"  class="form-control">
+                        @error('email')
+                        <label id="error-email"
+                               class="validation-error-label text-danger"
+                               for="email">{{ $message }}</label>
+                        @enderror
+                        </div>
                 </div>
             </div>
 
             <div class="row">
                 <div class="col-lg-6">
                     <div class="mb-3">
-                        <label class="form-label">Phone #</label>
-                        <input type="text" value="+99-99-9999-9999" class="form-control">
-                        <div class="form-text text-muted">+99-99-9999-9999</div>
+                        <label class="form-label">Họ</label>
+                        <input type="text" wire:model="last_name" placeholder="Nhập họ"  class="form-control">
+                        @error('last_name')
+                        <label id="error-last_name"
+                               class="validation-error-label text-danger"
+                               for="last_name">{{ $message }}</label>
+                        @enderror
                     </div>
                 </div>
-
                 <div class="col-lg-6">
                     <div class="mb-3">
-                        <label class="form-label">Upload profile image</label>
-                        <input type="file" class="form-control">
-                        <div class="form-text text-muted">Accepted formats: gif, png, jpg. Max file size 2Mb</div>
+                        <label class="form-label">Tên</label>
+                        <input type="text" wire:model="first_name" placeholder="Nhập tên"  class="form-control">
+                        @error('first_name')
+                        <label id="error-first_name"
+                               class="validation-error-label text-danger"
+                               for="first_name">{{ $message }}</label>
+                        @enderror
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="mb-3">
+                        <label class="form-label">Số điện thoại</label>
+                        <input type="text" wire:model="phone" placeholder="Nhập số điện thoại"  class="form-control">
+                        @error('phone')
+                        <label id="error-phone"
+                               class="validation-error-label text-danger"
+                               for="phone">{{ $message }}</label>
+                        @enderror
                     </div>
                 </div>
             </div>
 
+
             <div class="text-end">
-                <button type="submit" class="btn btn-primary">Save changes</button>
+                <button wire:loading wire:target="submit" class="btn btn-primary">
+                    <i class="ph-circle-notch spinner"></i>
+                    Lưu
+                </button>
+                <button wire:click="submit" wire:loading.remove class="btn btn-primary">
+                    <i class="ph-floppy-disk"></i>
+                    Lưu
+                </button>
             </div>
         </form>
     </div>
