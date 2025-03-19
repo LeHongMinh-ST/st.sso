d9ed256db7cd948bb011d0de19424550167e8ea2
minhlh <minhlh@haposoft.com>
2025-03-19 11:39:32 +0700

    fix: api

--- routes/api.php
+++ routes/api.php
@@ -2,21 +2,20 @@
 
 declare(strict_types=1);
 
-use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\Api\FacultyController;
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', fn (Request $request) => $request->user())->middleware('auth:api');
-Route::apiResource('user', UserController::class)
+Route::apiResource('users', UserController::class)
     ->only(['index', 'show'])
     ->middleware(['auth:api', 'check.superadmin.api']);
 
-Route::apiResource('faculty', FacultyController::class)
+Route::apiResource('faculties', FacultyController::class)
     ->only(['index'])
     ->middleware(['auth:api', 'check.superadmin.api']);
 
-Route::get('faculty/{faculty}/users', [FacultyController::class, 'getUsers'])
+Route::get('faculties/{faculty}/users', [FacultyController::class, 'getUsers'])
     ->middleware(['auth:api']);
-    Route::get('faculty/{faculty}/teachers', [FacultyController::class, 'getTeachers'])
-    ->middleware(['auth:api']);
+Route::get('faculties/{faculty}/teachers', [FacultyController::class, 'getTeachers'])->middleware(['auth:api']);
