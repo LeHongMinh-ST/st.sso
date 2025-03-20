2b3a46d31e45fd9f50bc7f24e771edcc1921c1a3
minhlh <minhlh@haposoft.com>
2025-03-20 15:53:04 +0700

    feat: check permission

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -4,11 +4,13 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers\Api;
 
+use App\Enums\Role;
 use App\Helpers\Constants;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\User\UserResource;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
@@ -27,6 +29,18 @@ class UserController extends Controller
 
     public function show(User $user)
     {
+        if (!Auth::guard('api')->user()->isSuperAdmin() && $user->faculty_id !== Auth::guard('api')->user()->faculty_id) {
+            return response()->json([
+                'message' => 'Forbidden',
+            ], 403);
+        }
+
+        if (Role::Student === Auth::guard('api')->user()->role) {
+            return response()->json([
+                'message' => 'Forbidden',
+            ], 403);
+        }
+
         return new UserResource($user);
     }
 }
