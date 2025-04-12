e2ce8f95bae1cdbdb5c0a4d202299540b85593d6
minhlh <minhhl298.st@gmail.com>
2025-04-12 15:48:26 +0700

    feat: fix class

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,6 @@
 
 declare(strict_types=1);
 
-use App\Http\Controllers\Admin\ClientController;
-use App\Http\Controllers\Admin\RoleController;
 use App\Http\Controllers\Api\FacultyController;
 use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
@@ -17,21 +15,6 @@ Route::middleware(['auth:api'])->group(function (): void {
         ->middleware(['api.permission:api.user']);
     Route::post('users/{user}/reset-password', [UserController::class, 'resetPassword'])
         ->middleware(['api.permission:api.user']);
-
-    // Faculty API
-    Route::apiResource('faculties', FacultyController::class)
-        ->only(['index', 'show'])
-        ->middleware(['api.permission:api.faculty']);
-
-    // Client API
-    Route::apiResource('clients', ClientController::class)
-        ->only(['index', 'show'])
-        ->middleware(['api.permission:api.client']);
-
-    // Role API
-    Route::apiResource('roles', RoleController::class)
-        ->only(['index', 'show'])
-        ->middleware(['api.permission:api.role']);
 });
 
 Route::apiResource('faculties', FacultyController::class)
