97eab3163c4ebd88a87d895793f86c6265cb74bc
minhlh-hapo <minhlh@haposoft.com>
2025-12-15 09:08:20 +0700

    fix: form json

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,6 +11,7 @@ use App\Http\Middleware\CheckSuperAdminApi;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 use Laravel\Passport\Http\Middleware\CheckClientCredentials;
 
 return Application::configure(basePath: dirname(__DIR__))
@@ -32,5 +33,11 @@ return Application::configure(basePath: dirname(__DIR__))
         ])->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
+        $exceptions->shouldRenderJsonWhen(function (Request $request) {
+            if ($request->is('api/*')) {
+                return true;
+            }
 
+            return $request->expectsJson();
+        });
     })->create();
